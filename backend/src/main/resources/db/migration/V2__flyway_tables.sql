c54a1e26e46484a1290ac3a49dd99a627e741924
rsmullick <shahriar.mullick@valpo.edu>
2026-06-14 07:10:48 +0900

    add modulith in properties

---
+++ backend/src/main/resources/db/migration/V2__flyway_tables.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS event_publication
+(
+    id                     UUID NOT NULL,
+    listener_id            TEXT NOT NULL,
+    event_type             TEXT NOT NULL,
+    serialized_event       TEXT NOT NULL,
+    publication_date       TIMESTAMP WITH TIME ZONE NOT NULL,
+    completion_date        TIMESTAMP WITH TIME ZONE,
+    status                 TEXT,
+    completion_attempts    INT,
+    last_resubmission_date TIMESTAMP WITH TIME ZONE,
+    PRIMARY KEY (id)
+    );
+
+CREATE TABLE IF NOT EXISTS event_publication_archive
+(
+    id                     UUID NOT NULL,
+    listener_id            TEXT NOT NULL,
+    event_type             TEXT NOT NULL,
+    serialized_event       TEXT NOT NULL,
+    publication_date       TIMESTAMP WITH TIME ZONE NOT NULL,
+    completion_date        TIMESTAMP WITH TIME ZONE,
+    status                 TEXT,
+    completion_attempts    INT,
+    last_resubmission_date TIMESTAMP WITH TIME ZONE,
+    PRIMARY KEY (id)
+    );
